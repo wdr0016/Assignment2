dd8a50c4cf0c583c11c526605ee7075a10455fb5
Will Riceu <wdr0016@auburn.edu>
2019-08-28 19:51:35 -0500

    Added extra tests

--- src/test/java/FinderTest.java
+++ src/test/java/FinderTest.java
@@ -10,21 +10,22 @@ public class FinderTest {
         int[] array1 = {2, 5, 9};
         int[] array2 = {-5, 9, 2};
         int[] array3 = {0, -8, -15};
+        int[] array4 = {1};
 
         assertEquals(new Integer(9), x.findMax(array1));
         assertEquals(new Integer(9), x.findMax(array2));
         assertEquals(new Integer(0), x.findMax(array3));
+        assertEquals(new Integer(1), x.findMax(array4));
     }
 
     @Test
-    public void test_edgeMinArray() {
+    public void test_edgeMaxArray() {
         Finder x = new Finder();
 
-        int[] nullArray = null;
-        assertNull(x.findMin(nullArray));
+        assertNull(x.findMax(null));
 
         int[] emptyArray = {};
-        assertNull(x.findMin(emptyArray));
+        assertNull(x.findMax(emptyArray));
     }
 
     @Test
@@ -33,20 +34,21 @@ public class FinderTest {
         int[] array1 = {2, 5, 2};
         int[] array2 = {-5, 9, 2};
         int[] array3 = {0, -8, -15};
+        int[] array4 = {1};
 
         assertEquals(new Integer(2), x.findMin(array1));
         assertEquals(new Integer(-5), x.findMin(array2));
         assertEquals(new Integer(-15), x.findMin(array3));
+        assertEquals(new Integer(1), x.findMin(array4));
     }
 
     @Test
-    public void test_edgeMaxArray() {
+    public void test_edgeMinArray() {
         Finder x = new Finder();
 
-        int[] nullArray = null;
-        assertNull(x.findMax(nullArray));
+        assertNull(x.findMin(null));
 
         int[] emptyArray = {};
-        assertNull(x.findMax(emptyArray));
+        assertNull(x.findMin(emptyArray));
     }
 }
